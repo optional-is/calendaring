8add7ba9e6be10ca9a916fc36917ee6c56e08d1f
Brian Suda <brian@suda.co.uk>
2014-08-14 08:32:37 +0000

    Added README.md files

--- A7-Quarterly/A7-generator.php
+++ A7-Quarterly/A7-generator.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set('Atlantic/Reykjavik');
 
-$year = 2014;
-$quarter = 3;
+$year = 2015;
+$quarter = 4;
 
 echo '<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'."\n";
---
+++ A7-Quarterly/README.md
@@ -0,0 +1,5 @@
+The A7-Generator will output a quarterly calendar which you print to fit, then cut and fold in half a few times. These are modeled from the pocketmod series of little booklets.
+
+Usage:
+php A7-generator.php > output.svg
+
